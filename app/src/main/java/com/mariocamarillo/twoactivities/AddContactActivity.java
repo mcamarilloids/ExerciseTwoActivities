7b187373f410034ff6193ecafb93ba21f7b913de
Mario Alberto Camarillo Salazar <mcamarilloids@gmail.com>
2018-09-27 00:44:57 -0500

    Se sube faltante del proyecto ya completo

--- app/src/main/java/com/mariocamarillo/twoactivities/AddContactActivity.java
+++ app/src/main/java/com/mariocamarillo/twoactivities/AddContactActivity.java
@@ -1,13 +1,91 @@
 package com.mariocamarillo.twoactivities;
 
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-public class AddContactActivity extends AppCompatActivity {
+public class AddContactActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextInputEditText tilName;
+    private TextInputEditText tilPhone;
+    private TextInputEditText tilEmail;
+    private TextInputEditText tilDescription;
+    private DatePicker dpDate;
+    private Button bNext;
+    private CoordinatorLayout cLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+        setViews();
+        setData();
+    }
+
+    private void setViews() {
+        tilName = findViewById(R.id.name);
+        tilPhone = findViewById(R.id.phone);
+        tilEmail = findViewById(R.id.email);
+        tilDescription = findViewById(R.id.contact_description);
+        dpDate = findViewById(R.id.datepicker);
+        bNext = findViewById(R.id.next);
+        cLayout = findViewById(R.id.parent);
+        bNext.setOnClickListener(this);
+    }
+
+    private void setData() {
+        if(getIntent().getStringExtra(getString(R.string.name)) != null){
+            tilName.setText(getIntent().getStringExtra(getString(R.string.name)));
+            tilPhone.setText(getIntent().getStringExtra(getString(R.string.phone)));
+            tilDescription.setText(getIntent().getStringExtra(getString(R.string.description)));
+            tilEmail.setText(getIntent().getStringExtra(getString(R.string.email)));
+            final int day, month, year;
+            day = Integer.parseInt(getIntent().getStringExtra(getString(R.string.birthday)).substring(0,2));
+            month = Integer.parseInt(getIntent().getStringExtra(getString(R.string.birthday)).substring(3,5))-1;
+            year = Integer.parseInt(getIntent().getStringExtra(getString(R.string.birthday)).substring(6,10));
+            dpDate.updateDate(year, month, day);
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.next:
+                if(tilName.getText().toString().isEmpty() || tilPhone.getText().toString().isEmpty() ||
+                        tilEmail.getText().toString().isEmpty() || tilDescription.getText().toString().isEmpty()){
+                    Snackbar.make(cLayout, "Ingresar todos los campos", Snackbar.LENGTH_LONG).show();
+                }else{
+                    final Intent intent = new Intent(this, ContactInformationActivity.class);
+                    intent.putExtra(getString(R.string.name), tilName.getText().toString());
+                    intent.putExtra(getString(R.string.phone), tilPhone.getText().toString());
+                    intent.putExtra(getString(R.string.email), tilEmail.getText().toString());
+                    intent.putExtra(getString(R.string.description), tilDescription.getText().toString());
+                    final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                    final Calendar calendar = Calendar.getInstance();
+                    calendar.set(Calendar.YEAR, dpDate.getYear());
+                    calendar.set(Calendar.MONTH, dpDate.getMonth());
+                    calendar.set(Calendar.DAY_OF_MONTH, dpDate.getDayOfMonth());
+                    Date valueDatePicket = calendar.getTime();
+                    final String sDate = dateFormatter.format(valueDatePicket);
+                    intent.putExtra(getString(R.string.birthday), sDate);
+                    startActivity(intent);
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/mariocamarillo/twoactivities/ContactInformationActivity.java
+++ app/src/main/java/com/mariocamarillo/twoactivities/ContactInformationActivity.java
@@ -1,13 +1,66 @@
 package com.mariocamarillo.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ContactInformationActivity extends AppCompatActivity {
+public class ContactInformationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvName;
+    private TextView tvDescription;
+    private TextView tvDate;
+    private TextView tvPhone;
+    private TextView tvEmail;
+    private Button bEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_information);
+        setViews();
+        setData();
+    }
+
+
+
+    private void setViews() {
+        tvName = findViewById(R.id.name);
+        tvDescription = findViewById(R.id.description);
+        tvDate = findViewById(R.id.date);
+        tvPhone = findViewById(R.id.phone);
+        tvEmail = findViewById(R.id.email);
+        bEdit = findViewById(R.id.edit);
+        bEdit.setOnClickListener(this);
+    }
+
+    private void setData() {
+        if(getIntent().getStringExtra(getString(R.string.name)) != null){
+            tvName.setText(getIntent().getStringExtra(getString(R.string.name)));
+            tvPhone.setText(getIntent().getStringExtra(getString(R.string.phone)));
+            tvDescription.setText(getIntent().getStringExtra(getString(R.string.description)));
+            tvEmail.setText(getIntent().getStringExtra(getString(R.string.email)));
+            tvDate.setText(getIntent().getStringExtra(getString(R.string.birthday)));
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.edit:
+                final Intent intent = new Intent(this, AddContactActivity.class);
+                intent.putExtra(getString(R.string.name), tvName.getText().toString());
+                intent.putExtra(getString(R.string.phone), tvPhone.getText().toString());
+                intent.putExtra(getString(R.string.email), tvEmail.getText().toString());
+                intent.putExtra(getString(R.string.description), tvDescription.getText().toString());
+                intent.putExtra(getString(R.string.birthday), tvDate.getText().toString());
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
